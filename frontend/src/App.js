b5f03c86c05340c4aeb0f0e54e298799e36e019b
Alex Huang Feng <alexhuang05@gmail.com>
2020-10-18 12:00:30 +0200

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,22 @@ class App extends Component {
 
     state = {
         data: [
-            ["COL", 10, 90, 20, 39, 70, 4, 1, 5, 2],
-            ["PER", 30, 50, 20, 90, 10, 2, 3, 2, 2],
-            ["ESP", 50, 70, 30, 25, 37, 1, 3, 5, 4],
-            ["MEX", 70, 30, 20, 75, 7, 3, 4, 2, 2],
-            ["ARG", 90, 80, 10, 39, 30, 5, 1, 1, 1]
+            // {
+            //     code: 'ARG',
+            //     name: 'Argentina',
+            //     avg: 3.2,
+            //     categories: {
+            //         "Atencion al cliente": {
+            //             rating: 3,
+            //             avg: 54
+            //         }
+            //     }
+            // }
+            // ["COL", 10, 90, 20, 39, 70, 4, 1, 5, 2],
+            // ["PER", 30, 50, 20, 90, 10, 2, 3, 2, 2],
+            // ["ESP", 50, 70, 30, 25, 37, 1, 3, 5, 4],
+            // ["MEX", 70, 30, 20, 75, 7, 3, 4, 2, 2],
+            // ["ARG", 90, 80, 10, 39, 30, 5, 1, 1, 1]
         ]
     }
     countryCodes = [
@@ -24,16 +35,45 @@ class App extends Component {
         { "code": 'MEX', "name": "Mexico" }
     ]
 
-    componentDidMount() {
+    createData(code, name, avg, clusterAvg, commentcounts) {
+        let categories = {};
+        for (const category in clusterAvg) {
+            if (clusterAvg.hasOwnProperty(category)) {
+                categories[category] = {
+                    rating: clusterAvg[category],
+                    avg: commentcounts.results[category] / commentcounts.total
+                }
+            }
+        }
+        return {
+            code: code,
+            name: name,
+            avg: avg.toFixed(0),
+            categories: categories
+        };
+    }
 
+    componentDidMount() {
+        let clusters = []
         this.countryCodes.forEach(country => {
-            fetch("http://3.137.101.89:3000/api/ratings?country=Argentina")
+            console.log("country", country.name);
+            fetch("http://3.137.101.89:3000/api/ratings?country=" + country.name)
                 .then(res => res.json())
                 .then(
                     (result) => {
                         console.log("result", result);
+                        let clusterAvg = result.cluster_avg;
+                        let globalSum = 0;
+                        for (const category in clusterAvg) {
+                            if (clusterAvg.hasOwnProperty(category)) {
+                                const element = clusterAvg[category];
+                                globalSum += element
+                            }
+                        }
+                        let avg = globalSum / Object.keys(clusterAvg).length;
+                        clusters.push(this.createData(country.code, country.name, avg, result.cluster_avg, result.comments_count))
                         this.setState({
-                            result
+                            data: clusters
                         });
                     },
                     (error) => {
@@ -47,7 +87,7 @@ class App extends Component {
         <div className='App'>
             <Header/>
             <div className='App-map'>
-            <ChoroplethMap data={this.state.data} />
+            <ChoroplethMap/>
             </div>
         
             <div className='interactive-table'>
